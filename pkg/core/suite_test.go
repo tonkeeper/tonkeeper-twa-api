5592192ea766233213fd18ecc1a8d29f5b58a7fc
aleksej.paschenko <aleksej.paschenko@gmail.com>
2023-09-29 15:45:01 +0300

    Add tests for bridge

---
+++ pkg/core/suite_test.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"context"
+
+	"github.com/tonkeeper/tongo/ton"
+
+	"github.com/tonkeeper/tonkeeper-twa-api/pkg/telegram"
+)
+
+type mockStorage struct {
+	OnUnsubscribeFromBridgeEvents func(ctx context.Context, userID telegram.UserID, clientID *ClientID) error
+	OnSubscribeToBridgeEvents     func(ctx context.Context, userID telegram.UserID, clientID ClientID, origin string) error
+}
+
+func (m *mockStorage) SubscribeToAccountEvents(ctx context.Context, userID telegram.UserID, account ton.Address) error {
+	return nil
+}
+
+func (m *mockStorage) GetAccountEventsSubscriptions(ctx context.Context) ([]AccountEventsSubscription, error) {
+	return nil, nil
+}
+
+func (m *mockStorage) UnsubscribeAccountEvents(ctx context.Context, userID telegram.UserID) error {
+	return nil
+}
+
+func (m *mockStorage) SubscribeToBridgeEvents(ctx context.Context, userID telegram.UserID, clientID ClientID, origin string) error {
+	return m.OnSubscribeToBridgeEvents(ctx, userID, clientID, origin)
+}
+
+func (m *mockStorage) UnsubscribeFromBridgeEvents(ctx context.Context, userID telegram.UserID, clientID *ClientID) error {
+	return m.OnUnsubscribeFromBridgeEvents(ctx, userID, clientID)
+}
+
+var _ Storage = (*mockStorage)(nil)
