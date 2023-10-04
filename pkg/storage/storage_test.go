07697c48ce52a97bdf3c033f6d39f82fcf695eac
aleksej.paschenko <aleksej.paschenko@gmail.com>
2023-10-04 12:03:09 +0300

    Add maxWalletsPerUser limit

--- pkg/storage/storage_test.go
+++ pkg/storage/storage_test.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/stretchr/testify/require"
+	"github.com/tonkeeper/tongo/ton"
 	"go.uber.org/zap"
 
 	"github.com/tonkeeper/tonkeeper-twa-api/pkg/core"
@@ -20,9 +21,14 @@ func initDatabase(pool *pgxpool.Pool, t *testing.T) {
 	require.Nil(t, err)
 	_, err = pool.Exec(context.Background(), "INSERT INTO twa.bridge_subscriptions (telegram_user_id, client_id, origin) VALUES (2, '2002', 'dns.ton.org')")
 	require.Nil(t, err)
+
+	_, err = pool.Exec(context.Background(), "INSERT INTO twa.subscriptions (telegram_user_id, account) VALUES (1, '0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1000')")
+	require.Nil(t, err)
+	_, err = pool.Exec(context.Background(), "INSERT INTO twa.subscriptions (telegram_user_id, account) VALUES (1, '0:6ccd325a858c379693fae2bcaab1c2906831a4e10a6c3bb44ee8b615bca1d220')")
+	require.Nil(t, err)
 }
 
-type subscriptionData struct {
+type bridgeSubscriptionData struct {
 	TelegramUserID telegram.UserID
 	ClientID       core.ClientID
 	Origin         string
@@ -35,7 +41,7 @@ func Test_storage_SubscribeToBridgeEvents(t *testing.T) {
 		clientID         core.ClientID
 		origin           string
 		maxSubscriptions int
-		wantData         []subscriptionData
+		wantData         []bridgeSubscriptionData
 		wantErr          string
 	}{
 		{
@@ -43,8 +49,8 @@ func Test_storage_SubscribeToBridgeEvents(t *testing.T) {
 			userID:           1,
 			clientID:         "1002",
 			origin:           "dex.ton",
-			maxSubscriptions: maxSubscriptionsPerUser,
-			wantData: []subscriptionData{
+			maxSubscriptions: maxBridgeSubscriptionsPerUser,
+			wantData: []bridgeSubscriptionData{
 				{TelegramUserID: 1, ClientID: "1000", Origin: "dns.ton.org"},
 				{TelegramUserID: 1, ClientID: "1001", Origin: "ton.org"},
 				{TelegramUserID: 1, ClientID: "1002", Origin: "dex.ton"},
@@ -56,8 +62,8 @@ func Test_storage_SubscribeToBridgeEvents(t *testing.T) {
 			userID:           1,
 			clientID:         "1005",
 			origin:           "dns.ton.org",
-			maxSubscriptions: maxSubscriptionsPerUser,
-			wantData: []subscriptionData{
+			maxSubscriptions: maxBridgeSubscriptionsPerUser,
+			wantData: []bridgeSubscriptionData{
 				{TelegramUserID: 1, ClientID: "1001", Origin: "ton.org"},
 				{TelegramUserID: 1, ClientID: "1005", Origin: "dns.ton.org"},
 				{TelegramUserID: 2, ClientID: "2002", Origin: "dns.ton.org"},
@@ -68,8 +74,8 @@ func Test_storage_SubscribeToBridgeEvents(t *testing.T) {
 			userID:           2,
 			clientID:         "2002",
 			origin:           "ton.org",
-			maxSubscriptions: maxSubscriptionsPerUser,
-			wantData: []subscriptionData{
+			maxSubscriptions: maxBridgeSubscriptionsPerUser,
+			wantData: []bridgeSubscriptionData{
 				{TelegramUserID: 1, ClientID: "1000", Origin: "dns.ton.org"},
 				{TelegramUserID: 1, ClientID: "1001", Origin: "ton.org"},
 				{TelegramUserID: 2, ClientID: "2002", Origin: "ton.org"},
@@ -89,9 +95,9 @@ func Test_storage_SubscribeToBridgeEvents(t *testing.T) {
 			pool := createDB(t)
 			initDatabase(pool, t)
 			s := &storage{
-				logger:                  zap.L(),
-				pool:                    pool,
-				maxSubscriptionsPerUser: tt.maxSubscriptions,
+				logger:                        zap.L(),
+				pool:                          pool,
+				maxBridgeSubscriptionsPerUser: tt.maxSubscriptions,
 			}
 			err := s.SubscribeToBridgeEvents(context.Background(), tt.userID, tt.clientID, tt.origin)
 			if tt.wantErr != "" {
@@ -104,9 +110,9 @@ func Test_storage_SubscribeToBridgeEvents(t *testing.T) {
 			require.Nil(t, err)
 			defer rows.Close()
 
-			data := make([]subscriptionData, 0)
+			data := make([]bridgeSubscriptionData, 0)
 			for rows.Next() {
-				var sub subscriptionData
+				var sub bridgeSubscriptionData
 				if err := rows.Scan(&sub.TelegramUserID, &sub.ClientID, &sub.Origin); err != nil {
 					require.Nil(t, err)
 				}
@@ -130,12 +136,12 @@ func Test_storage_UnsubscribeFromBridgeEvents(t *testing.T) {
 		name     string
 		userID   telegram.UserID
 		clientID *core.ClientID
-		wantData []subscriptionData
+		wantData []bridgeSubscriptionData
 	}{
 		{
 			name:   "remove all subscriptions for user",
 			userID: 1,
-			wantData: []subscriptionData{
+			wantData: []bridgeSubscriptionData{
 				{TelegramUserID: 2, ClientID: "2002", Origin: "dns.ton.org"},
 			},
 		},
@@ -143,7 +149,7 @@ func Test_storage_UnsubscribeFromBridgeEvents(t *testing.T) {
 			name:     "remove specific subscription for user",
 			userID:   1,
 			clientID: clientIDPtr("1000"),
-			wantData: []subscriptionData{
+			wantData: []bridgeSubscriptionData{
 				{TelegramUserID: 1, ClientID: "1001", Origin: "ton.org"},
 				{TelegramUserID: 2, ClientID: "2002", Origin: "dns.ton.org"},
 			},
@@ -156,6 +162,76 @@ func Test_storage_UnsubscribeFromBridgeEvents(t *testing.T) {
 			s := &storage{logger: zap.L(), pool: pool}
 			err := s.UnsubscribeFromBridgeEvents(context.Background(), tt.userID, tt.clientID)
 			require.Nil(t, err)
+		})
+	}
+}
+
+func Test_storage_SubscribeToAccountEvents(t *testing.T) {
+	tests := []struct {
+		name              string
+		userID            telegram.UserID
+		addr              ton.Address
+		maxWallets        int
+		wantSubscriptions []core.AccountEventsSubscription
+		wantErr           string
+	}{
+		{
+			name:       "all good",
+			userID:     2,
+			addr:       ton.Address{ID: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1999")},
+			maxWallets: 10,
+			wantSubscriptions: []core.AccountEventsSubscription{
+				{TelegramUserID: 1, Account: ton.MustParseAccountID("0:6ccd325a858c379693fae2bcaab1c2906831a4e10a6c3bb44ee8b615bca1d220")},
+				{TelegramUserID: 1, Account: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1000")},
+				{TelegramUserID: 2, Account: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1999")},
+			},
+		},
+		{
+			name:       "add already existing subscription",
+			userID:     1,
+			addr:       ton.Address{ID: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1000")},
+			maxWallets: 10,
+			wantSubscriptions: []core.AccountEventsSubscription{
+				{TelegramUserID: 1, Account: ton.MustParseAccountID("0:6ccd325a858c379693fae2bcaab1c2906831a4e10a6c3bb44ee8b615bca1d220")},
+				{TelegramUserID: 1, Account: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1000")},
+			},
+		},
+		{
+			name:       "max wallets per user reached",
+			userID:     1,
+			addr:       ton.Address{ID: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1999")},
+			maxWallets: 2,
+			wantErr:    `max wallets per user reached`,
+			wantSubscriptions: []core.AccountEventsSubscription{
+				{TelegramUserID: 1, Account: ton.MustParseAccountID("0:6ccd325a858c379693fae2bcaab1c2906831a4e10a6c3bb44ee8b615bca1d220")},
+				{TelegramUserID: 1, Account: ton.MustParseAccountID("0:bdf3fa8098d129b54b4f73b5bac5d1e1fd91eb054169c3916dfc8ccd536d1000")},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pool := createDB(t)
+			initDatabase(pool, t)
+			s := &storage{logger: zap.L(), pool: pool, maxWalletsPerUser: tt.maxWallets}
+			err := s.SubscribeToAccountEvents(context.Background(), tt.userID, tt.addr)
+			if tt.wantErr != "" {
+				require.EqualError(t, err, tt.wantErr)
+			} else {
+				require.Nil(t, err)
+			}
+
+			subs, err := s.GetAccountEventsSubscriptions(context.Background())
+			require.Nil(t, err)
+
+			sort.Slice(subs, func(i, j int) bool {
+				if subs[i].TelegramUserID != subs[j].TelegramUserID {
+					return subs[i].TelegramUserID < subs[j].TelegramUserID
+				}
+				return subs[i].Account.String() < subs[j].Account.String()
+			})
+
+			require.Equal(t, tt.wantSubscriptions, subs)
+
 		})
 	}
 }
